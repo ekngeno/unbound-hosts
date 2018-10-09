4f0c2196ef500b500e18254c6503c571bf87a57b
ekngeno <43566750+ekngeno@users.noreply.github.com>
2018-10-09 08:30:01 +0300

    Update ads.sh

--- ads.sh
+++ ads.sh
@@ -1,6 +1,6 @@
+#this script will block ads, fakenews, porn and gambling sites
 
-#!/bin/bash
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
 cat hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" refuse"}' > unbound_ad_servers
-mv unbound_ad_servers /etc/unbound/conf.d/unbound_ad_servers
+mv unbound_ad_servers /etc/unbound/unbound.conf.d/unbound_ad_servers
 systemctl restart unbound.service
